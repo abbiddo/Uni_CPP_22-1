61d446f2fb2d79fad9256563f6a2aec8e45f140d
abbiddo@naver.com <abbiddo@naver.com>
2022-05-19 13:14:13 +0900

    12주차 - 1 : 제네릭 template

---
+++ chap10/chap10/ex10-6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class MyStack {
+	int tos;
+	T data[100];
+public:
+	MyStack();
+	void push(T element);
+	T pop();
+};
+template <class T>
+MyStack<T>::MyStack() { tos = -1; }
+template <class T>
+void MyStack<T>::push(T element) { data[++tos] = element; }
+template <class T>
+T MyStack<T>::pop() { return data[tos--]; }
+
+int main() {
+	MyStack<int> iStack;
+	iStack.push(3);
+	cout << iStack.pop() << endl;
+
+	MyStack<double> dStack;
+	dStack.push(3.5);
+	cout << dStack.pop() << endl;
+
+	MyStack<char>* p = new MyStack<char>();
+	p->push('a');
+	cout << p->pop() << endl;
+	delete p;
+}
